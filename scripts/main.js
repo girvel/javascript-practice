1d3f04e5938b5de22c715f5d5c064435f67499cf
girvel <widauka@ya.ru>
2023-02-06 08:22:49 +0400

    Changing date

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
+const globals = {
+	birth_rate: 1 / (4 * 9),
+};
+
 class Kingdom {
 	constructor() {
 		this.resources = new Map(Object.entries({
@@ -5,6 +9,8 @@ class Kingdom {
 			people: 2,
 			food: 1
 		}));
+
+		this.day = 1;
 	}
 }
 
@@ -13,8 +19,15 @@ let kingdom = new Kingdom();
 // game cycle
 setInterval(() => {
 
+	// time system
+	kingdom.day += 3;
+
 	// display system
 	kingdom.resources.forEach((value, resource) => {
 		document.getElementById(`resource_${resource}`).textContent = value;
 	});
+
+	let month = Math.trunc(kingdom.day / 30);
+	document.getElementById("date").textContent 
+		= `1.${month % 12}.${Math.trunc(month / 12)}`
 }, 50);
